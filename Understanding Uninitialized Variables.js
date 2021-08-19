5987fa08cdbd41eeb8f02043a8210502d37aa1b5
Keshara1997 <kavindakeshara97@gmail.com>
2021-08-19 12:42:47 +0530

    Understanding Uninitialized Variables

---
+++ Understanding Uninitialized Variables.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+var a = 6;
+var b = 15;
+var c = "I am a String!";
