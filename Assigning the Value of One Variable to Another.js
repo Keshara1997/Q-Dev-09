83a48fbba3224943fa11d246de055dcc8f4d6aaf
Kavinda Keshara <63952119+Keshara1997@users.noreply.github.com>
2021-08-24 21:26:06 +0530

    Number (#5)
    * comment file
    
    * Variables
    
    * assigning
    
    * Understanding Uninitialized Variables
    
    * number and operatas

---
+++ Assigning the Value of One Variable to Another.js
@@ -0,0 +1,24 @@
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+
+b = a;
+
+var StUdLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+
+// Variable assignments
+
+StUdLyCapVaR = 10;
+properCamelCase = "A String";
+TitleCaseOver = 9000;
+
+var studlyCapVar;
+var titleCaseOver
+
+studlyCapVar = StUdLyCapVaR;
+titleCaseOver = TitleCaseOver;
