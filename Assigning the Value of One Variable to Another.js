c8499b5948d1f592c8df7385ef09885a866d8389
Keshara1997 <kavindakeshara97@gmail.com>
2021-08-24 21:19:05 +0530

    number and operatas

--- Assigning the Value of One Variable to Another.js
+++ Assigning the Value of One Variable to Another.js
@@ -6,3 +6,19 @@ var b;
 // Only change code below this line
 
 b = a;
+
+var StUdLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+
+// Variable assignments
+
+StUdLyCapVaR = 10;
+properCamelCase = "A String";
+TitleCaseOver = 9000;
+
+var studlyCapVar;
+var titleCaseOver
+
+studlyCapVar = StUdLyCapVaR;
+titleCaseOver = TitleCaseOver;
